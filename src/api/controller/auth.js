0aa814f663462c6c21f0f9f62b14306c99729bf0
fanleisong <fanleisong@163.com>
2021-06-19 14:03:56 +0800

    first commit

---
+++ src/api/controller/auth.js
@@ -0,0 +1,63 @@
+const Base = require('./base.js');
+
+module.exports = class extends Base {
+  async loginByWeixinAction() {
+    const code = this.post('code');
+    const fullUserInfo = this.post('userInfo');
+    const clientIp = this.ctx.ip;
+
+    if (typeof(fullUserInfo) == "undefined"){ 
+      const { errno, errmsg, data: userInfo } = await this.service('weixin', 'api').logincode(code);
+
+       
+      }else{
+    
+        const { errno, errmsg, data: userInfo } = await this.service('weixin', 'api').login(code, fullUserInfo);
+       
+        if (errno !== 0) {
+          return this.fail(errno, errmsg);
+        }
+
+        console.log("--"+ userInfo.openid)
+    // 根据openid查找用户是否已经注册
+    let userId = await this.model('user').where({ weixin_openid: userInfo.openid }).getField('id', true);
+    if (think.isEmpty(userId)) {
+      // 注册
+      userId = await this.model('user').add({
+        username: '微信用户' + think.uuid(6),
+        password: '',
+        register_time: parseInt(new Date().getTime() / 1000),
+        register_ip: clientIp,
+        mobile: '',
+        weixin_openid: userInfo.openId,
+        avatar: userInfo.avatarUrl || '',
+        gender: userInfo.gender || 1, // 性别 0：未知、1：男、2：女
+        nickname: userInfo.nickName
+      });
+    }
+
+    // 查询用户信息
+    const newUserInfo = await this.model('user').field(['id', 'username', 'nickname', 'gender', 'avatar', 'birthday']).where({ id: userId }).find();
+
+    // 更新登录信息
+    await this.model('user').where({ id: userId }).update({
+      last_login_time: parseInt(new Date().getTime() / 1000),
+      last_login_ip: clientIp
+    });
+
+    const TokenSerivce = this.service('token', 'api');
+    const sessionKey = await TokenSerivce.create({ user_id: userId });
+
+    if (think.isEmpty(sessionKey)) {
+      return this.fail('生成 token 失败');
+    }
+
+    return this.success({ token: sessionKey, userInfo: newUserInfo });
+      }
+    
+  }
+
+  async logoutAction() {
+    return this.success();
+  }
+};
---
+++ src/common/config/config.js
@@ -0,0 +1,18 @@
+// default config
+module.exports = {
+  port: 8046,
+  default_module: 'api',
+  weixin: {
+    appid: 'wxdefde16ace262c6d', // 小程序 appid
+    secret: '4ab38b4b55d020fbc19da8dfe6b02803', // 小程序密钥
+    mch_id: '', // 商户帐号ID
+    partner_key: '', // 微信支付密钥
+    notify_url: '' // 微信异步通知，例：https://www.nideshop.com/api/pay/notify
+  },
+  express: {
+    // 快递物流信息查询使用的是快递鸟接口，申请地址：http://www.kdniao.com/
+    appid: '', // 对应快递鸟用户后台 用户ID
+    appkey: '', // 对应快递鸟用户后台 API key
+    request_url: 'http://api.kdniao.cc/Ebusiness/EbusinessOrderHandle.aspx'
+  }
+};
